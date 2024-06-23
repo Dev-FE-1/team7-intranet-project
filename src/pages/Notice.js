4e23a50dd307d250b83feffe91dfc9e085e2178b
jizero <jizero27@gmail.com>
2024-06-23 23:51:07 +0900

    공지사항 레이아웃 작성 및 검색 input, 등록 버튼 구현

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,3 +1,22 @@
 export default function Notice(root) {
-  root.innerHTML = `<div class='notice'></div>`;
+
+  
+
+
+  root.innerHTML = `<div class='notice'><div class="notice__label">공지사항</div> 
+            <div class="notice__right">  
+              <div class="notice__search">
+                <input type="text" class="inputText notice__inputText" placeholder="검색어를 입력해주세요." />
+              </div>
+            <input type="button" value="등록" class="btn"/>
+            </div>
+            <div class="notice__container">
+            <div class="card__img">
+          <img src="/public/temp-image.jpg" alt="puppy"/>
+        </div>
+        <div class="card__title">
+          <p>공지입니다.</p>
+        </div>
+          </div>
+</div>`;
 }
