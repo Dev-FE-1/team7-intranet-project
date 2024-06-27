bf46fe704bf3e650f75fefecbb359be37f3aa685
jizero <jizero27@gmail.com>
2024-06-27 13:03:05 +0900

    공지사항 모달 UI에 공통 모달 함수 적용

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,3 +1,5 @@
+import { useModal } from "../js/common";
+
 export default function Notice(root) {
   root.innerHTML = `
             <div class="notice">  
@@ -12,22 +14,20 @@ export default function Notice(root) {
               </div>
             <div class="notice__container">
             </div>
-        </div>
-        <div class="noticeModal__card">
+            <div class="notice__modalCard">
+            </div>
         </div>
         `;
 
-  const noticeContainer=document.querySelector('.notice__container');
-  const noticeModal=document.querySelector('.noticeModal__card');
+  const noticeContainer=document.querySelector('.notice__container'); 
+  const noticeModal=document.querySelector('.notice__modalCard');
 
-  //카드 제목, 이미지, 이미지alt 값을 불러와서 html로 리턴해주는 함수
+  // 카드 id, 제목, 이미지 데이터를 불러와서 카드를 생성하는 함수
   function createCard(cardData){
-        const cardDatastr=JSON.stringify(cardData);
-
         return `
-          <div class="notice__card" card-data='${cardDatastr}'>
+          <div class="notice__card" data-id='${cardData.id}'>
             <div class="card__img">
-            <img src="${cardData.imgUrl}" alt="${cardData.imgAlt}"/>
+            <img src="${cardData.imgs}" alt="${cardData.imgAlt}"/>
             </div>
             <div class="card__title notice__title">
             <p>${cardData.title}</p>
@@ -36,7 +36,7 @@ export default function Notice(root) {
         `;
   }
 
-   // 배열에 들어있는 카드 제목, 이미지, 이미지 alt값을 불러와서 카드를 추가하는 함수
+  // 데이터에 들어있는 카드의 갯수 만큼 카드를 추가하는 함수
   function addCards(container, cardsData){
     let cards='';
     for(let cardData of cardsData){
@@ -46,69 +46,391 @@ export default function Notice(root) {
     
     const cardElements=container.querySelectorAll('.notice__card');
 
-    //모든 공지 카드의 요소를 하나씩 가져옴, 카드를 클릭하면 해당 카드에 대한 ShowModal 함수 호출
+  // 데이터 객체의 id와 카드의 id가 일치하면 해당 id의 모달 내용 출력
     cardElements.forEach((card)=>{
       card.addEventListener('click',()=>{
-        showModal(card);
+        const cardId=card.getAttribute('data-id');
+        const data=cardsData.find((el)=>el.id==cardId);
+        renderModal(data);
+        useModal([{btn:'notice__card', modal:'notice__modalCard'}])
       });
     });
   }
 
-  //클릭한 카드의 정보를 가져와 모달로 출력시키는 함수
-  function showModal(card){
-    //JSON문자열을 객체로 변환시키기 위한 parse
-    const cardData=JSON.parse(card.getAttribute('card-data'));
-
-      noticeModal.innerHTML= `
+  // 클릭한 카드의 정보를 가진 모달을 생성하는 함수 
+  function renderModal(data){
+      noticeModal.innerHTML= ` 
       <div class="modal modal--lg modal--bgWhite"> 
         <div class="modal__bb"></div>
-        <div class="modal__inner">
-          <div class="noticeModal__bar">
-          <p class="noticeModal__title">${cardData.title}</p>
-          <p class="noticeModal__date">2024-06-25:00:00</p>
+        <div class="modal__inner notice__modal__inner">
+          <div class="notice__modal__bar">
+          <p class="notice__modal__title">${data.title}</p>
+          <p class="notice__modal__date">${data.date}</p>
           <div>
           <div class="modal__content">
-            <div class="noticeModal__img">
-            <img src="${cardData.imgUrl}" alt="${cardData.imgAlt}"/>
+            <div class="notice__modal__img">
+            <img src="${data.imgs}" alt="${data.imgAlt}"/>
             </div>
-            <div class="noticeModal__content">${cardData.content}</div>
+            <div class="notice__modal__content">${data.content}</div>
           </div>
-            <div class="btn btn--notice--cancel">닫기</div>
+            <div class="btn btn--notice--cancel modalClose">닫기</div>
         </div>
       </div>
     `;
-
-    let closeBtn=document.querySelector('.btn--notice--cancel');
-    
-    //닫기 버튼 눌렀을 때 현재 출력되는 모달 숨기기  
-    closeBtn.addEventListener('click',()=>{
-    console.log('btn close')
-    noticeModal.classList.add('modal__hidden')
-    });
-    
-    //숨기기 로직이 끝난 후 hidden 클래스를 remove 시켜야 정상작동 
-    noticeModal.classList.remove('modal__hidden')
   }
-    
-  
 
   // 카드 데이터 객체 (임시)
   const cardData=[
-    {title:'이굿위크 이벤트 공지사항', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy' , content:'이거 데이터 언제 다 만들지'},
-    {title:'강아지 귀엽네요', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'장바구니 서비스 점검 일정', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', 
-    content: '데이터 길면 어떻게 나오는지 확인하려고 쓰는거임 동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 ...더보기 데이터 길면 어떻게 나오는지 확인하려고 쓰는거임 동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 ...더보기'
-    },
-    {title:'연차 및 복지 혜택 안내', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '연차 사용 관련 안내: 연차는 최대 30일까지 사용 가능하며, 사용 계획이나 추가 정보가 필요하시면 HR팀에 문의해 주세요.'},
-    {title:'받고', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'저메추', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'card7 test', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'card8 test', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'card9 test', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'card10 test', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'card11 test', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-    {title:'데이터 넣어야지..', imgUrl:'/public/temp-image.jpg', imgAlt:'puppy', content: '지피티 힘내라.. 야무지게 만들어줘'},
-  ];
-
+  {
+    "id": 1,
+    "date": "2024-06-26",
+    "title": "새로운 업데이트 공지",
+    "content": "2024년 6월 26일에 새로운 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 2,
+    "date": "2024-06-25",
+    "title": "정기 점검 안내",
+    "content": "2024년 6월 25일에 정기 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 3,
+    "date": "2024-06-24",
+    "title": "서비스 개선 사항",
+    "content": "2024년 6월 24일에 서비스 개선 사항이 적용됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 4,
+    "date": "2024-06-23",
+    "title": "이벤트 소식",
+    "content": "2024년 6월 23일부터 새로운 이벤트가 시작됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 5,
+    "date": "2024-06-22",
+    "title": "보안 업데이트 공지",
+    "content": "2024년 6월 22일에 보안 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 6,
+    "date": "2024-06-21",
+    "title": "긴급 점검 안내",
+    "content": "2024년 6월 21일에 긴급 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 7,
+    "date": "2024-06-20",
+    "title": "고객센터 운영 시간 변경",
+    "content": "2024년 6월 20일부터 고객센터 운영 시간이 변경됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 8,
+    "date": "2024-06-19",
+    "title": "서비스 일시 중단 안내",
+    "content": "2024년 6월 19일에 서비스가 일시 중단됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 9,
+    "date": "2024-06-18",
+    "title": "신규 기능 추가 안내",
+    "content": "2024년 6월 18일에 신규 기능이 추가됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 10,
+    "date": "2024-06-17",
+    "title": "서버 이전 작업 공지",
+    "content": "2024년 6월 17일에 서버 이전 작업이 있습니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 11,
+    "date": "2024-06-16",
+    "title": "정기 점검 안내",
+    "content": "2024년 6월 16일에 정기 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 12,
+    "date": "2024-06-15",
+    "title": "서비스 개선 사항",
+    "content": "2024년 6월 15일에 서비스 개선 사항이 적용됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 13,
+    "date": "2024-06-14",
+    "title": "이벤트 소식",
+    "content": "2024년 6월 14일부터 새로운 이벤트가 시작됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 14,
+    "date": "2024-06-13",
+    "title": "보안 업데이트 공지",
+    "content": "2024년 6월 13일에 보안 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 15,
+    "date": "2024-06-12",
+    "title": "긴급 점검 안내",
+    "content": "2024년 6월 12일에 긴급 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 16,
+    "date": "2024-06-11",
+    "title": "고객센터 운영 시간 변경",
+    "content": "2024년 6월 11일부터 고객센터 운영 시간이 변경됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 17,
+    "date": "2024-06-10",
+    "title": "서비스 일시 중단 안내",
+    "content": "2024년 6월 10일에 서비스가 일시 중단됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 18,
+    "date": "2024-06-09",
+    "title": "신규 기능 추가 안내",
+    "content": "2024년 6월 9일에 신규 기능이 추가됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 19,
+    "date": "2024-06-08",
+    "title": "서버 이전 작업 공지",
+    "content": "2024년 6월 8일에 서버 이전 작업이 있습니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 20,
+    "date": "2024-06-07",
+    "title": "정기 점검 안내",
+    "content": "2024년 6월 7일에 정기 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 21,
+    "date": "2024-06-06",
+    "title": "서비스 개선 사항",
+    "content": "2024년 6월 6일에 서비스 개선 사항이 적용됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 22,
+    "date": "2024-06-05",
+    "title": "이벤트 소식",
+    "content": "2024년 6월 5일부터 새로운 이벤트가 시작됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 23,
+    "date": "2024-06-04",
+    "title": "보안 업데이트 공지",
+    "content": "2024년 6월 4일에 보안 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 24,
+    "date": "2024-06-03",
+    "title": "긴급 점검 안내",
+    "content": "2024년 6월 3일에 긴급 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 25,
+    "date": "2024-06-02",
+    "title": "고객센터 운영 시간 변경",
+    "content": "2024년 6월 2일부터 고객센터 운영 시간이 변경됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 26,
+    "date": "2024-06-01",
+    "title": "서비스 일시 중단 안내",
+    "content": "2024년 6월 1일에 서비스가 일시 중단됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 27,
+    "date": "2024-05-31",
+    "title": "신규 기능 추가 안내",
+    "content": "2024년 5월 31일에 신규 기능이 추가됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 28,
+    "date": "2024-05-30",
+    "title": "서버 이전 작업 공지",
+    "content": "2024년 5월 30일에 서버 이전 작업이 있습니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 29,
+    "date": "2024-05-29",
+    "title": "정기 점검 안내",
+    "content": "2024년 5월 29일에 정기 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 30,
+    "date": "2024-05-28",
+    "title": "서비스 개선 사항",
+    "content": "2024년 5월 28일에 서비스 개선 사항이 적용됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 31,
+    "date": "2024-05-27",
+    "title": "이벤트 소식",
+    "content": "2024년 5월 27일부터 새로운 이벤트가 시작됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 32,
+    "date": "2024-05-26",
+    "title": "보안 업데이트 공지",
+    "content": "2024년 5월 26일에 보안 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 33,
+    "date": "2024-05-25",
+    "title": "긴급 점검 안내",
+    "content": "2024년 5월 25일에 긴급 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 34,
+    "date": "2024-05-24",
+    "title": "고객센터 운영 시간 변경",
+    "content": "2024년 5월 24일부터 고객센터 운영 시간이 변경됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 35,
+    "date": "2024-05-23",
+    "title": "서비스 일시 중단 안내",
+    "content": "2024년 5월 23일에 서비스가 일시 중단됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 36,
+    "date": "2024-05-22",
+    "title": "신규 기능 추가 안내",
+    "content": "2024년 5월 22일에 신규 기능이 추가됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 37,
+    "date": "2024-05-21",
+    "title": "서버 이전 작업 공지",
+    "content": "2024년 5월 21일에 서버 이전 작업이 있습니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 38,
+    "date": "2024-05-20",
+    "title": "정기 점검 안내",
+    "content": "2024년 5월 20일에 정기 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 39,
+    "date": "2024-05-19",
+    "title": "서비스 개선 사항",
+    "content": "2024년 5월 19일에 서비스 개선 사항이 적용됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 40,
+    "date": "2024-05-18",
+    "title": "이벤트 소식",
+    "content": "2024년 5월 18일부터 새로운 이벤트가 시작됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 41,
+    "date": "2024-05-17",
+    "title": "보안 업데이트 공지",
+    "content": "2024년 5월 17일에 보안 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 42,
+    "date": "2024-05-16",
+    "title": "긴급 점검 안내",
+    "content": "2024년 5월 16일에 긴급 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 43,
+    "date": "2024-05-15",
+    "title": "고객센터 운영 시간 변경",
+    "content": "2024년 5월 15일부터 고객센터 운영 시간이 변경됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 44,
+    "date": "2024-05-14",
+    "title": "서비스 일시 중단 안내",
+    "content": "2024년 5월 14일에 서비스가 일시 중단됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 45,
+    "date": "2024-05-13",
+    "title": "신규 기능 추가 안내",
+    "content": "2024년 5월 13일에 신규 기능이 추가됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 46,
+    "date": "2024-05-12",
+    "title": "서버 이전 작업 공지",
+    "content": "2024년 5월 12일에 서버 이전 작업이 있습니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 47,
+    "date": "2024-05-11",
+    "title": "정기 점검 안내",
+    "content": "2024년 5월 11일에 정기 점검이 있을 예정입니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 48,
+    "date": "2024-05-10",
+    "title": "서비스 개선 사항",
+    "content": "2024년 5월 10일에 서비스 개선 사항이 적용됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 49,
+    "date": "2024-05-09",
+    "title": "이벤트 소식",
+    "content": "2024년 5월 9일부터 새로운 이벤트가 시작됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  },
+  {
+    "id": 50,
+    "date": "2024-05-08",
+    "title": "보안 업데이트 공지",
+    "content": "2024년 5월 8일에 보안 업데이트가 진행됩니다.",
+    "imgs": "/public/temp-image.jpg"
+  }
+];
   addCards(noticeContainer, cardData);
 }
