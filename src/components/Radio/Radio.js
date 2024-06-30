73e8a5a33b17527a795621b8c1d1b21fd4164054
jizero <jizero27@gmail.com>
2024-06-30 14:51:57 +0900

    라디오 컴포넌트화

---
+++ src/components/Radio/Radio.js
@@ -0,0 +1,15 @@
+import './radio.css';
+// options=[{id, name, label, checked, disabled}] 
+// 아이디(1, type1), 공통으로 묶을 이름, 라디오 버튼명, 선택된상태 유무, 비활성화 상태 유무
+export default function radio(options){
+  const radioHtml=options.map((el)=>{
+    return `<div class="radioItem">
+      <label for="${el.id}">
+      <input id="${el.id}" name="${el.name}" type="radio" ${el.checked ? 'checked' : ''} ${el.disabled ? 'disabled' : ''}/>${el.label}
+      </label>
+      </div>
+      `;
+  }).join('');
+
+  return radioHtml;
+}
