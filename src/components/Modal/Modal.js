9f0df8b276d75e88c01a4113be310aa5f89e6a35
Haeun <haruyam15@gmail.com>
2024-07-01 16:21:07 +0900

    컴포넌트 클래스화

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,45 +5,65 @@
 //   buttons : 모달 버튼 Array [{버튼호출할때 필요한 객체값}]
 //   classList : 모달에 추가할 클래스(공백으로 구분) String  >> 'class1 class2'
 //   size : 모달 사이즈 'md' 또는 'lg' 또는 'full' String
+//   content : 모달 컨텐츠 String
 // }
 
-export default function Modal(props) {
-  const { name, trigger, title, buttons, classList = '', size = 'sm' } = props;
+export default class Modal {
+  constructor(props) {
+    const {
+      name,
+      trigger,
+      title,
+      buttons,
+      classList = '',
+      size = 'sm',
+      content,
+    } = props;
 
-  if (!name || !trigger) {
-    alert(`모달컴포넌트 사용 시 name, trigger는 필수 값입니다.
-    Modal({name : '모달이름', trigger : '모달버튼'})`);
-    return;
+    if (!name || !trigger) {
+      alert(`모달컴포넌트 사용 시 name, trigger는 필수 값입니다.
+        Modal({name : '모달이름', trigger : '모달버튼'})`);
+      return;
+    }
+
+    this.name = name;
+    this.trigger = trigger;
+    this.title = title;
+    this.buttons = buttons;
+    this.classList = classList;
+    this.size = size;
+    this.content = content;
   }
 
-  document.querySelector('#root').insertAdjacentHTML(
-    'beforeend',
-    `<div class="modal ${name} modal_${size} ${classList}">
+  useModal() {
+    const modal = document.querySelector(`.${this.name}`);
+    // 모달 열기
+    const btnOpen = document.querySelectorAll(`.${this.trigger}`);
+    btnOpen.forEach((btn) =>
+      btn.addEventListener('click', () => modal.classList.toggle('show'))
+    );
+    // 모달 닫기
+    modal.addEventListener('click', (e) => {
+      const btnClose = e.target.closest('.modalClose');
+      if (!btnClose) return;
+      e.target.closest('.modal').classList.toggle('show');
+    });
+  }
+
+  render() {
+    return `
+      <div class="modal ${this.name} modal_${this.size} ${this.classList}">
         <div class="modal_bb"></div>
         <div class="modal_inner">
-          ${title !== undefined ? `<p class="modal_title">${title}</p>` : ''}
-          <div class="modal_content"></div>
+          ${this.title ? `<p class="modal_title">${this.title}</p>` : ''}
+          <div class="modal_content">${this.content}</div>
           ${
-            //버튼 컴포넌트 완성되면 Button()수정해야 함
-            buttons
+            this.buttons
               ? `<div class="modal_btns"><button class="btn btn--light modalClose">취소</button><button class="btn modalClose">확인</button></div>`
               : ''
           }
         </div>
-      </div>`
-  );
-
-  const modal = document.querySelector(`.${name}`);
-  //모달 열기
-  const btnOpen = document.querySelectorAll(`.${trigger}`);
-  btnOpen.forEach((btn) =>
-    btn.addEventListener('click', () => modal.classList.toggle('show'))
-  );
-
-  //모달 닫기
-  modal.addEventListener('click', (e) => {
-    const btnClose = e.target.closest('.modalClose');
-    if (!btnClose) return;
-    e.target.closest('.modal').classList.toggle('show');
-  });
+      </div>
+    `;
+  }
 }
