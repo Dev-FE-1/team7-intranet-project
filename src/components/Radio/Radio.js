fc99d3a5e6bac3a170574a0a8546c0cd9e0a640f
jizero <jizero27@gmail.com>
2024-07-02 00:05:06 +0900

    라디오 컴포넌트 클래스화

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -1,14 +1,28 @@
 import './radio.css';
-// options=[{id, name, label, checked, disabled}] 
-// 아이디(1, type1), 공통으로 묶을 이름, 라디오 버튼명, 선택된상태 유무, 비활성화 상태 유무
-export default function radio(Props){
-  const radioHtml=Props.map((el)=>{
-    return `<div class="radioItem">
-      <label for="${el.id}">
-      <input id="${el.id}" name="${el.name}" type="radio" ${el.checked ? 'checked' : ''} ${el.disabled ? 'disabled' : ''}/>${el.label}
-      </label>
-      </div>
-      `;
-  }).join('');
-  return radioHtml;
+
+//id - 고유 id
+//name - 그룹으로 묶을 이름
+//label - 라디오 라벨
+//checked - 기본으로 체크 되어있는지 유무
+//disabled - 활성화/비활성화 유무
+
+// const radio1=new Radio([{id:1, name:'그룹1', label:'연차', checked:true, disabled:true},
+//                        {id:2, name:'그룹1', label:'배차', checked:true, disabled:false} ]);
+
+class Radio{
+    constructor(Props){
+        this.Props=Props;
+    }
+
+    render(){
+        return this.Props.map((p)=>`<div class="radioItem">
+        <label for="${p.id}">
+        <input id="${p.id}" name="${p.name}" type="radio" ${p.checked ? 'checked' : ''} ${p.disabled ? 'disabled' : ''}/>
+        ${p.label}
+        </label>
+        </div>`).join('') 
+        ;
+    }
 }
+
+export default Radio;
