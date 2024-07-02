d833c04e52b74ca7316ad556879fb197eb6d82ac
Haeun <haruyam15@gmail.com>
2024-07-02 20:52:44 +0900

    컴포넌트 수정

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,10 @@
+import Button from '/src/components/Button/Button.js';
+
 // {
 //   name : 모달 클래스명 String (*필수값)
-//   trigger : 모달 활성화 버튼 클래스명 String (*필수값)
+//   trigger : 모달 활성화 버튼 클래스명 String
 //   title : 모달 제목 String
-//   buttons : 모달 버튼 Array [{버튼호출할때 필요한 객체값}]
+//   buttons : 모달 버튼 Array [{label:'', type:'', classList: ''}]
 //   classList : 모달에 추가할 클래스(공백으로 구분) String  >> 'class1 class2'
 //   size : 모달 사이즈 'md' 또는 'lg' 또는 'full' String
 //   content : 모달 컨텐츠 String
@@ -20,9 +22,8 @@ export default class Modal {
       content,
     } = props;
 
-    if (!name || !trigger) {
-      alert(`모달 컴포넌트 사용 시 name, trigger는 필수 값입니다.
-        Modal({name : '모달이름', trigger : '모달버튼'})`);
+    if (!name) {
+      alert(`모달 컴포넌트 사용 시 name은 필수 값입니다.`);
       return;
     }
 
@@ -51,6 +52,13 @@ export default class Modal {
   }
 
   render() {
+    const buttonTemp = this.buttons
+      .map((button) => {
+        const btn = new Button(button);
+        return btn.render();
+      })
+      .join('');
+
     return `
       <div class="modal ${this.name} modal_${this.size}${
       this.classList ? ` ${this.classList}` : ''
@@ -59,11 +67,7 @@ export default class Modal {
         <div class="modal_inner">
           ${this.title ? `<p class="modal_title">${this.title}</p>` : ''}
           <div class="modal_content">${this.content || ''}</div>
-          ${
-            this.buttons
-              ? `<div class="modal_btns"><button class="btn btn--light modalClose">취소</button><button class="btn modalClose">확인</button></div>`
-              : ''
-          }
+          ${buttonTemp}
         </div>
       </div>
     `;
