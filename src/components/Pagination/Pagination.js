21baeff11a6a1d1e748ed6295b67c43102caf0ce
jizero <jizero27@gmail.com>
2024-07-02 20:08:06 +0900

    페이지네이션 컴포넌트 수정 완료

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -7,30 +7,25 @@ import './pagination.css';
 //const pagination페이지명 = new Pagination({Props값 넣어서 선언})
 
 class Pagination {
-  constructor(Props) {
-    const { pagingClass, totalCnt, dataPerPage, pagingPerPage, data} = Props;
-
-    this.pagingClass = pagingClass;
+  constructor(props) {
+    const {totalCnt, currentPage, dataPerPage, pagingPerPage} = props;
+    
     this.totalCnt = totalCnt;
-    this.dataPerPage = dataPerPage;
+    this.currentPage=currentPage || 1;
+    this.dataPerPage = dataPerPage || 15;
     this.pagingPerPage = pagingPerPage;
-    this.data = data;
-
-    this.currentPage=1; //페이지 초기값
-    this.renderPagination(this.currentPage);
-    //this.showList(this.currentPage);
   }
 
-  //화면에 표시될 데이터 값에 맞게 페이지네이션을 렌더링 시켜주는 함수
-  renderPagination(currentPage) {
+  //데이터 값에 맞게 페이지네이션을 렌더링 시켜주는 함수
+  render() {
     //전체 목록 갯수에 따라서 보여질 페이지네이션
     let totalCount = Math.ceil(this.totalCnt / this.dataPerPage);
-    //페이지네이션 그룹 
-    let pageGroup = Math.ceil(currentPage / this.pagingPerPage); 
-    //그룹 내 마지막 페이지
-    let lastPage = Math.min(pageGroup * this.pagingPerPage, totalCount); 
+     //페이지네이션 그룹
+    let pageGroup = Math.ceil(this.currentPage / this.pagingPerPage); 
+     //그룹 내 마지막 페이지
+    let lastPage = Math.min(pageGroup * this.pagingPerPage, totalCount);
     //그룹 내 첫번째 페이지
-    let firstPage = Math.max((pageGroup - 1) * this.pagingPerPage + 1, 1); 
+    let firstPage = Math.max((pageGroup - 1) * this.pagingPerPage + 1, 1);
 
     let pagingHtml = `
       <ul class="pagination">
@@ -38,9 +33,8 @@ class Pagination {
         <li class="pagination_arrow" id="prev"><a href="#">◀</a></li>`;
 
     for (let i = firstPage; i <=lastPage; i++) {
-      console.log(i, currentPage)
       pagingHtml += `
-        <li class="pagination_num ${i === currentPage ? 'active' : ''}" data-id="${i}"><a href="#">${i}</a></li>
+        <li class="pagination_num ${i === this.currentPage ? 'active' : ''}" data-id="${i}"><a href="/}">${i}</a></li>
       `;
     }
 
@@ -49,68 +43,50 @@ class Pagination {
         <li class="btn btn--end" id="last"><a href="#">마지막</a></li>
       </ul>
     `;
-
-    document.querySelector(this.pagingClass).innerHTML = pagingHtml;
-
-    //첫번재 페이지로 이동
-    document.getElementById('first').addEventListener('click', () => { 
-      this.gotoPage(1) },{capture: true});
-    //이전 그룹으로 이동 
-    document.getElementById('prev').addEventListener('click', () => {
-      const prevGroupLastPage = Math.max(currentPage - this.pagingPerPage, 1);
-      this.gotoPage(prevGroupLastPage),{capture: true};
-    });
-
-    // 다음 그룹으로 이동
-    document.getElementById('next').addEventListener('click', (e) => {
-      console.log(e)
-      const nextGroupFirstPage = currentPage + this.pagingPerPage
-      this.gotoPage(nextGroupFirstPage <= totalCount ? nextGroupFirstPage : totalCount);
-    }),{capture: true};
-    
-    // 마지막 페이지로 이동
-    document.getElementById('last').addEventListener('click', (e) => {
-      console.log(e) 
-      this.gotoPage(totalCount)}),true;
-    
-    // 페이지네이션 숫자를 클릭하면 해당 숫자 페이지로 이동시킴
-    document.querySelectorAll('.pagination_num').forEach(page => {
-      page.addEventListener('click', (e) => {
-        console.log(e)
-      let pageNum = Number(e.currentTarget.getAttribute('data-id'));
-      this.gotoPage(pageNum);
-      });
-    }, true);
+  return pagingHtml;
   }
 
-  //목록 불러오기 테스트용 
-  // showList(page) {
-  //   const listContainer = document.querySelector('.list_container');
+    //이전, 다음, 처음, 마지막 버튼을 클릭했을 때 해당하는 페이지로 이동시키는 함수
+    usePagination(){
+      let totalCount = Math.ceil(this.totalCnt / this.dataPerPage);
+      let pageGroup = Math.ceil(this.currentPage / this.pagingPerPage); 
 
-  //   if (listContainer !== null) {
-  //     const startIdx = (page - 1) * this.dataPerPage;
-  //     const endIdx = Math.min(startIdx + this.dataPerPage, this.totalCnt);
-  //     const pageData = this.data.slice(startIdx, endIdx);
-
-  //     let listHtml = '<ul>';
-  //     pageData.forEach(item => {
-  //       listHtml += `<li>${item.TITLE} - ${item.WRITE_DATE}</li>`;
-  //     });
-  //     listHtml += '</ul>';
-
-  //     listContainer.innerHTML = listHtml;
-  //   } else {
-  //     console.log('List container not found');
-  //   }
-  // }
+      //첫번재 페이지로 이동
+      document.getElementById('first').addEventListener('click', () => { 
+        this.gotoPage(1) });
+      //이전 그룹으로 이동 
+      document.getElementById('prev').addEventListener('click', () => {
+        const prevGroupLastPage = Math.max((pageGroup - 1) * this.pagingPerPage, 1);
+        this.gotoPage(prevGroupLastPage);
+      });
+  
+      // 다음 그룹으로 이동
+      document.getElementById('next').addEventListener('click', (e) => {
+        const nextGroupFirstPage = pageGroup * this.pagingPerPage + 1;
+        this.gotoPage(nextGroupFirstPage <= totalCount ? nextGroupFirstPage : totalCount);
+      });
+      
+      // 마지막 페이지로 이동
+      document.getElementById('last').addEventListener('click', (e) => {
+        this.gotoPage(totalCount)});
+      
+      // 페이지네이션 숫자를 클릭하면 해당 숫자 페이지로 이동시킴
+      document.querySelectorAll('.pagination_num').forEach(page => {
+        page.addEventListener('click', (e) => {
+        let pageNum = Number(e.currentTarget.getAttribute('data-id'));
+        this.gotoPage(pageNum);
+        });
+      });
+    }
 
   //페이지네이션 내 버튼을 클릭했을 때 버튼 액션에 해당하는 페이지 값을 얻어와서 페이지네이션 렌더링
-  gotoPage(page) {
-    const totalCount = Math.ceil(this.totalCnt / this.dataPerPage);
-    if (page < 1 || page > totalCount) return;
-    this.renderPagination(page);
-    //this.showList(page);
+    gotoPage(page) {
+      const totalCount = Math.ceil(this.totalCnt / this.dataPerPage);
+      if (page < 1 || page > totalCount) return;
+      this.currentPage=page;
+      document.querySelector('.pagination_container').innerHTML = this.render(page);
+      this.usePagination();
+    }
   }
-}
 
 export default Pagination;
