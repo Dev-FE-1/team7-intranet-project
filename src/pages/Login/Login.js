8d1d0ee6af0bc8020a43eadeab5ea13b806252bd
Haeun <haruyam15@gmail.com>
2024-07-02 13:52:53 +0900

    로그인페이지UI

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,24 @@
-export default function Login() {
-  return `
-    <div class="login">
-    </div>
-  `;
+import './Login.css';
+
+export default function Login(root) {
+  root.innerHTML = `<div class="login">
+        <div class="login_box">
+          <h1 class="logo">
+            <img src="public/assets/images/logo.png" alt="77CM" />
+          </h1>
+          <form class="login_form" action="">
+            <input
+              type="text"
+              class="inputText"
+              placeholder="이메일을 입력해주세요."
+            />
+            <input
+              type="text"
+              class="inputText"
+              placeholder="비밀번호를 입력해주세요."
+            />
+            <button class="btn">로그인</button>
+          </form>
+        </div>
+      </div>`;
 }
