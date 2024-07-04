f6607463ecf548296e6ff0147ee26220265e6a68
Gin Song <sjgaru@gmail.com>
2024-07-05 02:51:59 +0900

    modal

--- src/pages/Employee/Employee.js
+++ src/pages/Employee/Employee.js
@@ -1,53 +1,60 @@
 import './Employee.css';
-import Card from '../../components/Card/Card.js';
-import Table from '../../components/Table/Table.js';
-import Pagination from '../../components/Pagination/Pagination.js';
+import Input from '../../components/Input/Input';
 
 export default function Employee(root) {
-  fetch('/api/employee/list')
-    .then((response) => response.json())
-    .then((data) => {
-      const employeeList = data['data'];
+  const data = [
+    {
+      userId: '1',
+      name: '김철수',
+      dept: '개발부',
+      position: '대리',
+      birth: '1990-01-01',
+      email: 'asdf@adf.com',
+      phone: '010-1234-5678',
+      img: 'server/images/profile/24.jpg',
+    },
+  ];
 
-      console.log(employeeList);
-
-      const table = new Table({
-        headers: ['사원번호', '이름', '부서', '직급', '이메일', '전화번호'],
-        data: employeeList.map((emp) => [
-          emp.userId,
-          `<div class="name_column">
-        <div class="thumbnail"><img src="${emp.img}" class="thumbnail" ></div> 
-        <div class="name">${emp.name}</div>
-          </div>`,
-          emp.dept,
-          emp.position,
-          emp.email,
-          emp.phone,
-        ]),
-        classList: 'table_employee',
-      });
-
-      const pagination = new Pagination({
-        totalCnt: employeeList.length,
-        currentPage: 1,
-        dataPerPage: 10,
-        pagingPerPage: 5,
-      });
-
-      const card = new Card({
-        page: {
-          title: '휴가/외출 관리',
-          content: `
-            <div class="listTable">${table.render()}</div>
-            <div class="pagination">${pagination.render()}</div>
-            `,
-        },
-      });
-
-      root.innerHTML = `<div>${card.render()}</div>`;
-    })
-
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  root.innerHTML = `
+      
+      <div class="employee_modal">
+                  <div class="modal_employeeImage">
+                    <div class="employeeImage">
+                      <img src=${data[0].img} class="employeeImage"/>
+                    </div>
+                  </div>
+                  <form class="employeeForm">
+                    <div class="employeeForm_group">
+                      <label for="employee_userID">사원번호</label>
+                      <input readonly type="text" id="employee_id" value='${data[0].userId}' />
+                    </div>
+                    <div class="employeeForm_group">
+                      <label for="employee_name">이름</label>
+                      <input readonly type="text" id="employee_name" value=${data[0].name} />
+                    </div>
+                    <div class="employeeForm_group">
+                      <label for="employee_dept">부서</label>
+                      <input readonly type="text" id="employee_dept" value=${data[0].dept} />
+                    </div>
+                    <div class="employeeForm_group">
+                      <label for="employee_position">직급</label>
+                      <input readonly type="text" id="employee_position" value=${data[0].position} />
+                    </div>
+                    <div class="employeeForm_group">
+                      <label for="employee_birth">생년월일</label>
+                      <input type="date" id="employee_birth" value=${data[0].birth} />
+                    </div>
+                    <div class="employeeForm_group">
+                      <label for="employee_email">이메일</label>
+                      <input readonly type="email" id="employee_email" value=${data[0].email} />
+                    </div>
+                    <div class="employeeForm_group">
+                      <label for="employee_phone">전화번호</label>
+                      <input type="tel" id="employee_phone" value=${data[0].phone} />
+                    </div>
+                  </form>
+                </div>
+      </div>
+      
+      `;
 }
